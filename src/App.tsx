5fe21b5e6f5be756c273b2218a5413f338171385
Gerald Haxhi <gerald.haxhi@softup.co>
2022-02-08 11:22:44 +0100

    Installed UI kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import { ReactElement } from 'react';
+import { Button } from 'antd';
 import './App.css';
 
-function App() {
+const App = (): ReactElement => {
+  const onButtonClick = (): void => {
+    console.log('Cliked');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button
+        onClick={onButtonClick}
+        type="default"
+        danger
+      >
+        Click me!
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
